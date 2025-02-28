f4d30e92ea0ff2751a908d6a0067b70311bb4ebd
Carlos <carlosferser03@gmail.com>
2025-02-28 01:22:26 +0100

    Nuevas clases sin terminar

--- ServidorApiRestaurante/Models/Cliente.cs
+++ ServidorApiRestaurante/Models/Cliente.cs
@@ -1,7 +1,42 @@
-﻿namespace ServidorApiRestaurante.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ServidorApiRestaurante.Models
 {
     public class Cliente
     {
+        [Key] // Clave primaria
+        public virtual int Id { get; set; }
+
+        [Required] // La propiedad 'Nombre' es obligatoria
+        [Column(TypeName = "TEXT")]
+        public virtual string Nombre { get; set; } = string.Empty;
+
+        [Required] // La propiedad 'Dni' es obligatoria
+        [Column(TypeName = "TEXT")]
+        public virtual string Dni { get; set; } = string.Empty;
+
+        [Column(TypeName = "VARCHAR(15)")]
+        public virtual string Num_Teléfono { get; set; } = string.Empty;
+
+
+        public Cliente() {}
+        
+        public Cliente(string Nombre, string Dni, string num_Teléfono)
+        {
+            this.Nombre = Nombre;
+            this.Dni = Dni;
+            this.Num_Teléfono = num_Teléfono;
+        }
+
+        public Cliente(int Id, string Nombre, string Dni, string num_Teléfono)
+        {
+            this.Id = Id;
+            this.Nombre = Nombre;
+            this.Dni = Dni;
+            this.Num_Teléfono = num_Teléfono;
+        }
+
 
     }
 }
--- ServidorApiRestaurante/Models/Mesa.cs
+++ ServidorApiRestaurante/Models/Mesa.cs
@@ -1,6 +1,58 @@
-﻿namespace ServidorApiRestaurante.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ServidorApiRestaurante.Models
 {
     public class Mesa
     {
+        [Key] // Clave primaria
+        public virtual int Id { get; set; }
+
+        [Required] // La propiedad 'Nombre' es obligatoria
+        [Column(TypeName = "TEXT")] //Float
+        public virtual float PosX { get; set; }
+
+        [Required] // La propiedad 'Nombre' es obligatoria
+        [Column(TypeName = "TEXT")] //Float
+        public virtual float PosY { get; set; }
+
+        [Required] // La propiedad 'Nombre' es obligatoria
+        [Column(TypeName = "TEXT")] //Float
+        public virtual float ScaleX { get; set; }
+
+        [Required] // La propiedad 'Nombre' es obligatoria
+        [Column(TypeName = "TEXT")] //Float
+        public virtual float ScaleY { get; set; }
+
+        [Required] // La propiedad 'Dni' es obligatoria
+        [Column(TypeName = "BOOL")]
+        public virtual bool Disponible { get; set; }
+
+        [Column(TypeName = "INTEGER")]
+        public virtual Restaurante? Restaurante { get; set; }
+
+
+        public Mesa() { }
+
+        public Mesa(float PosX, float PosY, float ScaleX, float ScaleY, bool Disponible, Restaurante Restaurante)
+        {
+            this.PosX = PosX;
+            this.PosY = PosY;
+            this.ScaleY = ScaleY;
+            this.ScaleY = ScaleY;
+            this.Disponible = Disponible;
+            this.Restaurante = Restaurante;
+        }
+
+        public Mesa(int Id, float PosX, float PosY, float ScaleX, float ScaleY, bool Disponible, Restaurante Restaurante)
+        {
+            this.Id = Id;
+            this.PosX = PosX;
+            this.PosY = PosY;
+            this.ScaleY = ScaleY;
+            this.ScaleY = ScaleY;
+            this.Disponible = Disponible;
+            this.Restaurante = Restaurante;
+        }
     }
 }
